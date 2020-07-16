05f60d39cbf604a0caeb82c78a9ff9fff80e0d23
“ze <1852749815@qq.com>
2020-07-16 17:22:08 +0800

    初始化用户服务

--- src/main/java/com/athaitao/scw/ScwUserApplication.java
+++ src/main/java/com/athaitao/scw/ScwUserApplication.java
@@ -2,12 +2,11 @@ package com.athaitao.scw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 public class ScwUserApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ScwUserApplication.class, args);
 	}
