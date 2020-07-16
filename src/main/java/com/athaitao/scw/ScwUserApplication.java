d7c0f821e5815a68f80c660840d6edb9670b7408
“ze <1852749815@qq.com>
2020-07-16 16:37:46 +0800

    初始化用户服务

---
+++ src/main/java/com/athaitao/scw/ScwUserApplication.java
@@ -0,0 +1,13 @@
+package com.athaitao.scw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScwUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScwUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athaitao/scw/ScwUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athaitao.scw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScwUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
