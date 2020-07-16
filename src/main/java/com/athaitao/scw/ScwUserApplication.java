7c1573f1d95877a9384d0dbc40cb6b6bcd76cd26
“ze <1852749815@qq.com>
2020-07-16 16:42:31 +0800

    修改用户模块

--- src/main/java/com/athaitao/scw/ScwUserApplication.java
+++ src/main/java/com/athaitao/scw/ScwUserApplication.java
@@ -3,6 +3,8 @@ package com.athaitao.scw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class ScwUserApplication {
 
